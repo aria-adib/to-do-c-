2b91df8e63d8c8fdfe6a9d0ea843efa5cd4bd8e2
aria-adib <150343001+aria-adib@users.noreply.github.com>
2024-01-24 18:53:19 +0330

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TODO_LIST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string Task = textBox1.Text.ToString();
+            string todayTask = textBox2.Text.ToString();
+            string Reminder = textBox3.Text.ToString();
+
+
+            if (checkBox1.Checked)
+            {
+                      listBox1.Items.Add(Task);
+                      listBox1.Items.Add(todayTask);
+
+
+            } else
+               
+                  {
+                   listBox2.Items.Add(Reminder);
+
+                }
+
+            
+            
+
+            textBox1.Clear();
+            textBox1.Focus();
+
+            textBox2.Clear();
+            textBox2.Focus();
+
+
+            textBox3.Clear();
+            textBox3.Focus();
+
+
+
+        }
+    }
+
+      
+    }
